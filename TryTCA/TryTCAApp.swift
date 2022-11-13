d87d49293a662a9ce4e0c63d7af708bcfe6a6b94
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-11-14 02:36:13 +0900

    Initial Commit

---
+++ TryTCA/TryTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryTCAApp.swift
+//  TryTCA
+//
+//  Created by toya on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct TryTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
