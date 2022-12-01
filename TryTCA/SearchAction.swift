3f656cfba545ca8d6251e819aa16e82ed813cea5
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-12-02 04:29:21 +0900

    並び順を説明しやすくする。

--- TryTCA/SearchAction.swift
+++ TryTCA/SearchAction.swift
@@ -4,18 +4,18 @@
 import ComposableArchitecture
 
 enum SearchAction: Equatable {
-    /// 天気を取得した
-    case forecastResponse(GeocodingSearch.Result.ID, TaskResult<Forecast>)
-    
     /// 検索キーワードを入力するテキストフィールドの内容が変わった
     case searchQueryChanged(String)
-    
+
     /// 検索キーワードが確定した
     case searchQueryChangeDebounced
 
-    /// 検索結果を取得した
+    /// 検索結果を取得した。TaskResultには例外を含めることができる。
     case searchResponse(TaskResult<GeocodingSearch>)
 
     /// 検索結果としての地名がタップされた
     case searchResultTapped(GeocodingSearch.Result)
+
+    /// 天気を取得した
+    case forecastResponse(GeocodingSearch.Result.ID, TaskResult<Forecast>)
 }
